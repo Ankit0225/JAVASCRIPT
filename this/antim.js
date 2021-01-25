3e1075fd18723c8ee28aca294b6e5925036ae961
Ankit <ankitmaurya0225@gmail.com>
2021-01-25 12:40:43 +0530

    THIS KEYWORD IN JS
    Signed-off-by: Ankit <ankitmaurya0225@gmail.com>

--- this/antim.js
+++ this/antim.js
@@ -1,1 +1,20 @@
-console.log("Hello I am Antim. I love panda and vamacharya");
+function checkthis() {
+    console.log(this)
+}
+
+let obj = {
+    a : 12,
+    b : 'asdsa',
+    c: 'ankit',
+    d: true,
+    e: function () {
+        console.log(this)
+    },
+    f: {
+        l: 234,
+        m: 'antim',
+        n: function () {
+            console.log(this)
+        },
+    },
+}
