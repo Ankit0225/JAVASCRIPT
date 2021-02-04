ff4867902e2acf097d7ba995b129c01aa65d0dd5
Ankit <ankitmaurya0225@gmail.com>
2021-02-04 14:17:20 +0530

    String-literals
    Signed-off-by: Ankit <ankitmaurya0225@gmail.com>

---
+++ string-literals/script.js
@@ -0,0 +1,24 @@
+  // He said ,"He is awesome!"
+
+  let s1 = 'He said ,"He is awesome!"'
+
+  let s2 = 'He said ,\"He is awesome!\"'
+
+  let s3 = "He said ,'He is awesome!'"
+
+  let s4 = `He said ,"He is awesome!"` //backticks
+
+  let s5 = "He said \n He is awesome!"
+
+  let s6 = `
+  He said 
+ "He is awesome!"
+  `
+  let name = 'Ankit'
+  
+  let g1 = 'Good Morning ' + name
+
+  let g2 = `Good Morning ${name}`
+
+  let s7 = `10 + 2 = ${10 + 2}`
+
