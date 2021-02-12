81a48c4710e1b027efb11ada87678792a9f8ed29
Ankit <ankitmaurya0225@gmail.com>
2021-02-12 09:37:11 +0530

    javascript notes
    Signed-off-by: Ankit <ankitmaurya0225@gmail.com>

---
+++ javascript lectures notes/prototypes/prototypes.js
@@ -0,0 +1,14 @@
+
+let str = 'Ankit Maurya'
+let num = 2508
+let boolean = true
+let arr = [333,345,212,33]
+let obj = {a:10,
+b: 'Sneaker'
+}
+
+// If x and y are not primitiive
+//  x and y : true <-- what does this mean ?
+//  this means they are reference to the same object in memory
+
+console.log(typeof String)
