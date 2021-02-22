f50669c460771b2826230b499df37de41ade894a
Ankit <ankitmaurya0225@gmail.com>
2021-02-22 12:45:19 +0530

    Pass by Value

---
+++ javascript lectures notes/Pass by Value/index.js
@@ -0,0 +1,19 @@
+var winners = "Ankit Maurya";
+
+function changewinner(winner){
+    console.log("Original winner = " + winner);
+    winner = "Aditya"
+    console.log("Changed winner = " + winner);
+}
+
+console.log("The winner is " + winners);
+
+changewinner(winners);
+
+console.log("Now the Winner is " + winners);
+
+
+
+
+
+
