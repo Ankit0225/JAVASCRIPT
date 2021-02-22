0b7afda669f5f33cb7a88708d62da443b1c33ecc
Ankit <ankitmaurya0225@gmail.com>
2021-02-22 12:45:51 +0530

    Pass by Referencce

---
+++ javascript lectures notes/Pass by Referencce/index.js
@@ -0,0 +1,20 @@
+var quiditchwinners = {
+    "Harry" , "Ron" , "Hermoine"
+} 
+ 
+  
+
+function changewinner(winners){
+    
+        winners[0] = "Draco";
+        winners[1] = "Chabbe";
+        winners[2] = "Pansy";
+
+}
+        console.log("The winners are = " + quiditchwinners);
+
+changewinner(quiditchwinners)
+
+console.log("Now the winners are = "+ quiditchwinners);
+
+// majorily it is pass by value
